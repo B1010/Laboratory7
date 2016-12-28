719524ada3dfeb995f3d0d0326c077a4973052bd
Артём Акопьян <artemik-98@outlook.com>
2016-12-28 22:04:36 +0400

    Добавлена лаба 5

--- Method/Program.cs
+++ Method/Program.cs
@@ -6,6 +6,302 @@ namespace Method
 {
     public class lab5
     {
+        public int[] exe1_minandmax(int[,] iarray)
+        {
+            int[] minamdmax = new int[1];
+
+            for (int i = 0; i < iarray.Length; i++)
+            {
+                for (int j = 0; j < iarray.Length; j++)
+                {
+                    iarray[i, j] = random.Next(10);
+                }
+            }
+
+            minamdmax[0] = iarray[0, 0];
+            for (int i = 0; i < iarray.GetLength(0); i++)
+            {
+                for (int j = 0; j < iarray.GetLength(1); j++)
+                {
+                    if (minamdmax[0] < iarray[i, j])
+                    {
+                        minamdmax[0] = iarray[i, j];
+                    }
+                }
+            }
+
+            minamdmax[1] = iarray[0, 0];
+            for (int i = 0; i < iarray.GetLength(0); i++)
+            {
+                for (int j = 0; j < iarray.GetLength(1); j++)
+                {
+                    if (minamdmax[1] > iarray[i, j])
+                    {
+                        minamdmax[1] = iarray[i, j];
+                    }
+                }
+            }
+            return minamdmax;
+        }
+        public int exe2_minamdmaxsumm(int[] iarray)
+        {
+            int sum = 0;
+            int min = iarray[0];
+            foreach (int i in iarray)
+            {
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+            int max = iarray[0];
+            foreach (int i in iarray)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            int MinIndex = 0, MaxIndex = 0;
+            for (int i = 0; i < iarray.Length; i++)
+            {
+                if (iarray[i] == min)
+                    MinIndex = i;
+                if (iarray[i] == max)
+                    MaxIndex = i;
+            }
+
+            if (MaxIndex > MinIndex)
+            {
+                for (int i = MinIndex + 1; i <= MaxIndex - 1; i++)
+                {
+                    sum = sum + iarray[i];
+                }
+                return sum;
+            }
+            else
+            {
+                return sum = 0;
+            }
+        }
+        public int exe3_digitmaxline(int[] iarray)
+        {
+            int max = 0;
+
+            for (int i = 0, temp2 = 1; i <= iarray.Length - 1; i++)
+            {
+                if (i != iarray.Length - 1)
+                {
+                    if (iarray[i] < iarray[i + 1])
+                        temp2++;
+                    else
+                    {
+                        if (temp2 > max)
+                            max = temp2;
+                        temp2 = 1;
+                    }
+                }
+                else
+                {
+                    if (temp2 > max)
+                        max = temp2;
+                }
+            }
+            return max;
+        }
+        public int[] exe4_arrayincluder(int[] iarray, int[] jarray)
+        {
+            int index = 1, count = 0;
+            
+            int[] larray = new int[iarray.Length + jarray.Length], tarray = new int[iarray.Length + jarray.Length];
+            
+            for (int i = 0; i < iarray.Length; i++)
+            {
+                larray[i] = iarray[i];
+            }
+            for (int i = 0; i < jarray.Length; i++)
+            {
+                larray[iarray.Length + i] = jarray[i];
+            }
+
+            for (int i = 0; i < larray.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < larray.Length; j++)
+                {
+                    if (larray[j] < larray[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = larray[min] = larray[i];
+            }
+            tarray[0] = larray[0];
+            for (int i = 1; i < larray.Length; i++)
+            {
+                if (larray[i] != larray[i - 1])
+                {
+                    tarray[index] = larray[i];
+                    index++;
+                }
+                else count++;
+            }
+            return tarray;
+        }
+        public int exe5_minofdigitsstartandens(int[] iarray)
+        {
+            int sum = 0;
+
+            int min = sum = iarray[0] + iarray[iarray.Length - 1];
+            for (int i = 0; i < iarray.Length; i++)
+            {
+                sum = iarray[i] + iarray[iarray.Length - 1 - i];
+                if (sum < min)
+                    min = sum;
+            }
+            return min;
+        }
+        public int exe6_summdigitsofthree(int[] iarray)
+        {
+            int sum = iarray[0] + iarray[1] + iarray[2];
+            for (int i = 1; i < iarray.Length - 2; i++)
+            {
+                if (sum < (iarray[i] + iarray[i + 1] + iarray[i + 2]))
+                {
+                    sum = iarray[i] + iarray[i + 1] + iarray[i + 2];
+                }
+            }
+            return sum;
+        }
+        public int[,] exe7_fromarraytoarray(int[,] iarray, int[] vector)
+        {
+            int[,] result = new int[iarray.GetLength(0), iarray.GetLength(1)];
+            
+            for (int i = 0; i < iarray.GetLength(1); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < iarray.GetLength(1); j++)
+                {
+                    result[i, j] = iarray[vector[i] - 1, j];
+                }
+            }
+            return result;
+        }
+        public int[] exe8_linecolum_maxandmin(int[,] iarray)
+        {
+            int k = 0;
+            int[] arrayline = new int[iarray.GetLength(0)];
+            int[] arraycolu = new int[iarray.GetLength(1)];
+            int[] maxandmin = new int[1];
+
+            if (iarray.GetLength(0) != iarray.GetLength(1))
+            {
+                return maxandmin;
+            }
+
+            for (int i = 0, sum = 0; i < iarray.GetLength(0); i++, k++)
+            {
+                for (int j = 0; j < iarray.GetLength(0); j++)
+                {
+                    sum += iarray[i, j];
+                }
+                arrayline[k] = sum;
+                sum = 0;
+            }
+            maxandmin[0] = arrayline[0];
+            for (int g = 0; g < iarray.GetLength(0); g++)
+            {
+                if (maxandmin[0] < arrayline[g])
+                {
+                    maxandmin[0] = arrayline[g];
+                }
+            }
+            for (int i = 0, f = 0, sum = 0; i < iarray.GetLength(1); i++, f++)
+            {
+                for (int j = 0; j < iarray.GetLength(1); j++)
+                {
+                    sum += iarray[j, i];
+                }
+                arraycolu[f] = sum;
+                sum = 0;
+            }
+            maxandmin[1] = arraycolu[0];
+            for (int b = 0; b < iarray.GetLength(1); b++)
+            {
+                if (maxandmin[1] < arraycolu[b])
+                {
+                    maxandmin[1] = arraycolu[b];
+                }
+
+            }
+            return maxandmin;
+        }
+        public int[,] exe10_AtoT(int[,] iarray)
+        {
+            Random randomforarray = new Random();
+            Console.Write("Введите размер матрицы: ");
+            int arraysize = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < iarray.GetLength(0); i++)
+            {
+                for (int j = i; j < iarray.GetLength(1); j++)
+                {
+                    int temp = iarray[i, j];
+                    iarray[i, j] = iarray[j, i];
+                    iarray[j, i] = temp;
+                }
+            }
+            return iarray;
+        }
+        public int[] exe11_sortandrevers(int[] iarray)
+        {
+            for (int i = 0; i < iarray.Length; i++)
+            {
+                for (int j = 0; j < iarray.Length - 1; j++)
+                {
+                    if (iarray[j] > iarray[j + 1])
+                    {
+                        int z = iarray[j];
+                        iarray[j] = iarray[j + 1];
+                        iarray[j + 1] = z;
+                    }
+                }
+            }
+            Array.Reverse(iarray);
+            return iarray;
+        }
+        public int[,] exe12_9x9to3x3(int[,] iarray)
+        {
+            int newi, newj;
+            int[,] jarray = new int[3, 3];
+
+            if (iarray.GetLength(0) != 9 && iarray.GetLength(1) != 9)
+            {
+                return jarray;
+            }
+
+            for (int i = 0; i < iarray.GetLength(0); i++)
+            {
+                for (int j = 0; j < iarray.GetLength(1); j++)
+                {
+                    if (i <= 2)
+                        newi = 0;
+                    else
+                    {
+                        if (i > 2 && i <= 5) newi = 1;
+                        else newi = 2;
+                    }
+                    if (j <= 2) newj = 0;
+                    else
+                    {
+                        if (j > 2 && j <= 5) newj = 1;
+                        else newj = 2;
+                    }
+                    jarray[newi, newj] = jarray[newi, newj] + iarray[i, j];
+                }
+            }
+            return jarray;
+        }
     }
     public class lab6
     {
